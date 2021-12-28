ddfb82ab6376fe0da7630f99c140646b2469dcf3
Weixiaojun <36564758+Weixiaojun666@users.noreply.github.com>
2021-12-28 22:09:44 +0800

    Update gettime.php

--- gettime.php
+++ gettime.php
@@ -4,7 +4,7 @@
 	$name =$_COOKIE['name'];
 	$id =$_COOKIE['id'];
 	$paper =$_COOKIE['paper'];
-	$conn=mysqli_connect('rm-2ze8d57a30h9j734tho.mysql.rds.aliyuncs.com','exam','hv2SuPc#kCL3K7M','exam','3306');
+	$conn=mysqli_connect();
 	
 	$sql="select ztime from times where id='$id' and paper='$paper' ;";
 	$result=mysqli_query($conn,$sql);
